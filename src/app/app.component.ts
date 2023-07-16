964f69f04b156f7287e915a3c2ed66cdcbf1a551
rafaeljgomezg <rafaeljgomezg@gmail.com>
2023-07-15 22:41:05 -0500

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
+  constructor(){
+
+  }
+  
+  ngOnInit(): void {
+    
+  }
+
   title = 'mini-calculadora';
+
+
+  numero1=0
+  numero2=0
+  resultado=0
+
+  sumar(){
+    this.resultado=this.numero1+this.numero2
+    
+  }
+  restar(){
+    this.resultado=this.numero1-this.numero2
+  }
+
+  multiplicar(){
+    this.resultado=this.numero1*this.numero2
+  }
+
 }
